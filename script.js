7973df83c72a5d71558b0fe2d6f987482f8cfd5f
Ciprian Turcu <turcuciprian1@gmail.com>
2017-04-17 23:38:46 +0300

    improved link pasting user experience

--- script.js
+++ script.js
@@ -1,58 +1,58 @@
 jQuery(document).ready(function() {
-  function writeUserData(xKey) {
-      database.ref(xKey+'/').set({
-          "val": "Empty",
-      });
-      readUserData(xKey);
-  }
-  function readUserData(xKey){
-    var starCountRef = firebase.database().ref(xKey+'/');
-    starCountRef.on('value', function(snapshot) {
-      var returningData = snapshot.val().val;
-      sResult = returningData;
-      if(returningData.indexOf('http')!=-1){
-        sResult = '<a href="'+returningData+'">'+returningData+'</a>';
-      }
-      codeCode.html(sResult);
-    });
-  }
+    function writeUserData(xKey) {
+        database.ref(xKey + '/').set({
+            "val": "Text from the iPhone Clipboard will show here...",
+        });
+        readUserData(xKey);
+    }
 
+    function readUserData(xKey) {
+        var starCountRef = firebase.database().ref(xKey + '/');
+        starCountRef.on('value', function(snapshot) {
+            var returningData = snapshot.val().val;
+            sResult = returningData;
+            if (returningData.indexOf('http') != -1) {
+                window.open(
+                    returningData,
+                    '_blank' // <- This is what makes it open in a new window.
+                );
+                sResult = '<a href="' + returningData + '" target="_blank">' + returningData + '</a>';
+            }
+            codeCode.html(sResult);
+        });
+    }
 
+    var sendCode = $("#sendCode");
+    var getCode = $("#getCode");
+    var codeCode = $('#codecode');
+    var stringData = $("#stringData");
+    var stringDataStr = '';
 
+    //send the text
+    if (sendCode[0]) {
+        sendCode.click(function() {
+            if (stringData[0]) {
+                stringDataStr = stringData.val();
+            }
+            writeUserData(stringDataStr);
 
+        });
+    }
+    //get the text
 
-  var sendCode = $("#sendCode");
-  var getCode = $("#getCode");
-  var codeCode = $('#codecode');
-  var stringData = $("#stringData");
-  var stringDataStr = '';
+    if (getCode[0]) {
+        getCode.click(function() {
+            readUserData();
 
-  //send the text
-  if (sendCode[0]) {
-      sendCode.click(function() {
-          if (stringData[0]) {
-              stringDataStr = stringData.val();
-          }
-          console.log('sending data: ' + stringDataStr);
-          writeUserData(stringDataStr);
+        });
 
-      });
-  }
-  //get the text
-
-  if (getCode[0]) {
-      getCode.click(function() {
-          readUserData();
-
-      });
-
-  }
+    }
 
     //QR code generating code:
 
     // set unique number
     var d = new Date();
-    var uniqueKey = d.getFullYear()+''+d.getMonth()+d.getDate()+d.getHours()+d.getMinutes()+d.getSeconds()+d.getMilliseconds()+'-'+Math.floor(Math.random() * (100 - 0)) + 0;
+    var uniqueKey = d.getFullYear() + '' + d.getMonth() + d.getDate() + d.getHours() + d.getMinutes() + d.getSeconds() + d.getMilliseconds() + '-' + Math.floor(Math.random() * (100 - 0)) + 0;
 
     // initialize and show QR code
     var qrcode = new QRCode("QRCode", {
@@ -65,7 +65,7 @@ jQuery(document).ready(function() {
     });
 
 
-writeUserData(uniqueKey);
+    writeUserData(uniqueKey);
 
 
 
