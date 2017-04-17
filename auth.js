b79d655d4fd7208d2e89fd4e9321e5561ca6a02a
Ciprian Turcu <turcuciprian1@gmail.com>
2017-04-17 21:06:31 +0300

    finished the web app - working 1.0 MVP

---
+++ auth.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyC-8ucejkD4Wbcr0ErmAD-f4k52K2HiQuY",
+  authDomain: "swishare-ba9b9.firebaseapp.com",
+  databaseURL: "https://swishare-ba9b9.firebaseio.com",
+  projectId: "swishare-ba9b9",
+  storageBucket: "swishare-ba9b9.appspot.com",
+  messagingSenderId: "433401920022"
+};
+firebase.initializeApp(config);
+var database = firebase.database();
---
+++ script.js
@@ -0,0 +1,70 @@
+jQuery(document).ready(function() {
+  function writeUserData(xKey) {
+      database.ref(xKey+'/').set({
+          "val": "Empty",
+      });
+      readUserData(xKey);
+  }
+  function readUserData(xKey){
+    var starCountRef = firebase.database().ref(xKey+'/');
+    starCountRef.on('value', function(snapshot) {
+      var returningData = snapshot.val().val;
+      console.log(returningData);
+      codeCode.html(returningData);
+    });
+  }
+
+
+
+
+
+  var sendCode = $("#sendCode");
+  var getCode = $("#getCode");
+  var codeCode = $('#codecode');
+  var stringData = $("#stringData");
+  var stringDataStr = '';
+
+  //send the text
+  if (sendCode[0]) {
+      sendCode.click(function() {
+          if (stringData[0]) {
+              stringDataStr = stringData.val();
+          }
+          console.log('sending data: ' + stringDataStr);
+          writeUserData(stringDataStr);
+
+      });
+  }
+  //get the text
+
+  if (getCode[0]) {
+      getCode.click(function() {
+          readUserData();
+
+      });
+
+  }
+
+    //QR code generating code:
+
+    // set unique number
+    var d = new Date();
+    var uniqueKey = d.getFullYear()+''+d.getMonth()+d.getDate()+d.getHours()+d.getMinutes()+d.getSeconds()+d.getMilliseconds()+'-'+Math.floor(Math.random() * (100 - 0)) + 0;
+
+    // initialize and show QR code
+    var qrcode = new QRCode("QRCode", {
+        text: uniqueKey,
+        width: 300,
+        height: 300,
+        colorDark: "#000000",
+        colorLight: "#ffffff",
+        correctLevel: QRCode.CorrectLevel.H
+    });
+
+
+writeUserData(uniqueKey);
+
+
+
+
+});
